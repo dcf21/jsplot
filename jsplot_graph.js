873dfd3745fb7fc38f225fd686ebf30d4e761d75
Dominic Ford <dcf21-github@dcford.org.uk>
2019-07-06 01:49:09 +0100

    Migrated eps_plot_canvas.c

--- jsplot_graph.js
+++ jsplot_graph.js
@@ -151,8 +151,7 @@ JSPlot_Axis.prototype.getPosition = function (x_in, allowOffBounds) {
 };
 
 // What is the value of this axis at point xin, in the range 0 (left) to 1 (right)?
-JSPlot_Axis.prototype.invGetPosition = function(x_in)
-{
+JSPlot_Axis.prototype.invGetPosition = function (x_in) {
     if (this.workspace.logFinal) {
         // Either linear...
         return this.workspace.minFinal + x_in * (this.workspace.maxFinal - this.workspace.minFinal);
@@ -160,4 +159,112 @@ JSPlot_Axis.prototype.invGetPosition = function(x_in)
         // ... or logarithmic
         return this.workspace.minFinal * pow(this.workspace.maxFinal / this.workspace.minFinal, x_in);
     }
+};
+
+// Does a value fall within the span of this axis?
+JSPlot_Axis.prototype.inRange = function (x_in) {
+    var x_min_set = false, x_max_set = false;
+    var x_min = 0, x_max = 0;
+
+    if (this.hardMin !== null) {
+        x_min_set = true;
+        x_min = this.hardMin;
+    }
+    if (this.hardMax !== null) {
+        x_max_set = true;
+        x_max = this.hardMax;
+    }
+    if (this.min === null) {
+        x_min_set = false;
+    }
+    if (this.max === null) {
+        x_max_set = false;
+    }
+
+    if (this.rangeReversed) {
+        var tmp = x_min_set;
+        x_min_set = x_max_set;
+        x_max_set = tmp;
+        tmp = x_min;
+        x_min = x_max;
+        x_max = tmp;
+    }
+
+    if (x_min_set && x_max_set) return (((x_in >= x_min) && (x_in <= x_max)) || ((x_in <= x_min) && (x_in >= x_max)));
+    if (x_min_set) return (x_in > x_min);
+    if (x_max_set) return (x_in < x_max);
+    return true; // Axis range is not fixed
+};
+
+JSPlot_Graph.prototype.project_3d = function (xap, yap, zap, zdepth) {
+    var width = this.width;
+    var height = this.width * this.aspect;
+
+    var x = width * (xap - 0.5);
+    var y = height * (yap - 0.5);
+    var z = zdepth * (zap - 0.5);
+
+    var x2 = x * Math.cos(this.viewAngleXY * Math.PI / 180) + y * Math.sin(this.viewAngleXY * Math.PI / 180);
+    var y2 = -x * Math.sin(this.viewAngleXY * Math.PI / 180) + y * Math.cos(this.viewAngleXY * Math.PI / 180);
+    var z2 = z;
+
+    var x3 = x2;
+    var y3 = y2 * Math.cos(this.viewAngleYZ * Math.PI / 180) - z2 * Math.sin(this.viewAngleYZ * Math.PI / 180);
+    var z3 = y2 * Math.sin(this.viewAngleYZ * Math.PI / 180) + z2 * Math.cos(this.viewAngleYZ * Math.PI / 180);
+
+    return {
+        "xpos": this.origin[0] + x3,
+        "ypos": this.origin[1] + z3,
+        "depth": y3
+    };
+};
+
+JSPlot_Graph.prototype.project_point = function (xin, yin, zin, axis_x, axis_y, axis_z, allowOffBounds) {
+    var width = this.width;
+    var height = this.width * this.aspect;
+    var output = [];
+
+    // Convert (xin,yin,zin) to axis positions on the range of 0-1
+    var xap = axis_x.getPosition(xin, 1);
+    var yap = axis_y.getPosition(yin, 1);
+    var zap = 0.5;
+
+    if (this.threeDimensional) zap = axis_z.getPosition(zin, 1);
+
+    if ((!isFinite(xap)) || (!isFinite(yap)) || (!isFinite(zap))) {
+        return {'xpos': NaN, 'ypos': NaN};
+    }
+    // Crop axis positions to range 0-1
+    if ((!allowOffBounds) && ((xap < 0) || (xap > 1) || (yap < 0) || (yap > 1) || (zap < 0) || (zap > 1))) {
+        return {'xpos': NaN, 'ypos': NaN};
+    }
+
+    // 3D plots
+    if (this.threeDimensional) {
+        output = this.project_3d(xap, yap, zap, zdepth);
+        var theta_x = Math.atan2(Math.cos(this.viewAngleXY * Math.PI / 180),
+            -Math.sin(this.viewAngleXY * Math.PI / 180) * Math.sin(this.viewAngleYZ * Math.PI / 180));
+        var theta_y = Math.atan2(Math.sin(this.viewAngleXY * Math.PI / 180),
+            Math.cos(this.viewAngleXY * Math.PI / 180) * Math.sin(this.viewAngleYZ * Math.PI / 180));
+        var theta_z = Math.atan2(0, Math.cos(this.viewAngleYZ * Math.PI / 180));
+
+        if (!isFinite(theta_x)) theta_x = 0.0;
+        if (!isFinite(theta_y)) theta_y = 0.0;
+        if (!isFinite(theta_z)) theta_z = 0.0;
+
+        output['theta_x'] = theta_x;
+        output['theta_y'] = theta_y;
+        output['theta_z'] = theta_z;
+    } else // 2D plots
+    {
+        output['xpos'] = this.origin[0] + width * xap;
+        output['ypos'] = this.origin[1] + height * yap;
+        output['depth'] = 0.0;
+
+        output['theta_x'] = Math.PI / 2;
+        output['theta_y'] = 0.0;
+        output['theta_z'] = 0.0;
+    }
+
+    return output;
 };
