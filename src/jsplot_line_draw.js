2bd4119da3bc293d9a8f270906d6e717f0b632b4
Dominic Ford <dcf21@dhcp-10-249-166-246.eduroam.wireless.private.cam.ac.uk>
2019-08-29 17:23:43 +0100

    Added comments to LineDraw class

--- src/jsplot_line_draw.js
+++ src/jsplot_line_draw.js
@@ -1,5 +1,16 @@
 // jsplot_line_draw.js
 
+/**
+ * JSPlot_LineDraw - Class for drawing a line across a graph, clipping it to the plot area.
+ * @param page {JSPlot_Canvas} - The canvas that this graph is being drawn onto
+ * @param graph {JSPlot_Graph} - The graph that this line is being drawn onto
+ * @param axis_x {JSPlot_Axis} - The x axis that each point on this line is referenced against
+ * @param axis_y {JSPlot_Axis} - The y axis that each point on this line is referenced against
+ * @param axis_z {JSPlot_Axis} - The z axis that each point on this line is referenced against
+ * @param color {JSPlot_Color} - The color to use when stroking this line
+ * @param lineWidth {number} - The width of line to draw
+ * @constructor
+ */
 function JSPlot_LineDraw(page, graph, axis_x, axis_y, axis_z, color, lineWidth) {
     this.page = page;
     this.graph = graph;
@@ -12,57 +23,109 @@ function JSPlot_LineDraw(page, graph, axis_x, axis_y, axis_z, color, lineWidth) 
     this.pt1 = null;
 }
 
+/**
+ * findCrossingPoints - Take a line segment, and clip it to the plot area.
+ * @param x1 {number} - In canvas pixel coordinates, the x position of the start of the line
+ * @param y1 {number} - In canvas pixel coordinates, the y position of the start of the line
+ * @param z1 {number} - In canvas pixel coordinates, the z position of the start of the line
+ * @param xap1 {number} - The fractional position along the x axis of the start point (in range 0 to 1)
+ * @param yap1 {number} - The fractional position along the y axis of the start point (in range 0 to 1)
+ * @param zap1 {number} - The fractional position along the z axis of the start point (in range 0 to 1)
+ * @param x2 {number} - In canvas pixel coordinates, the x position of the end of the line
+ * @param y2 {number} - In canvas pixel coordinates, the y position of the end of the line
+ * @param z2 {number} - In canvas pixel coordinates, the z position of the end of the line
+ * @param xap2 {number} - The fractional position along the x axis of the end point (in range 0 to 1)
+ * @param yap2 {number} - The fractional position along the y axis of the end point (in range 0 to 1)
+ * @param zap2 {number} - The fractional position along the z axis of the end point (in range 0 to 1)
+ * @returns {{Inside1: boolean, NCrossings: number, Inside2: boolean,
+ *            cy1: number, cz2: number, cz1: number, cx2: number, cx1: number, cy2: number}}
+ */
 JSPlot_LineDraw.prototype.findCrossingPoints = function (x1, y1, z1, xap1, yap1, zap1, x2, y2, z2, xap2, yap2, zap2) {
 
-    // If either point is inside canvas, set clip-region crossings to be the points themselves
+    var inside1 = (xap1 >= 0.0) && (xap1 <= 1.0) && (yap1 >= 0.0) && (yap1 <= 1.0) && (zap1 >= 0.0) && (zap1 <= 1.0);
+    var inside2 = (xap2 >= 0.0) && (xap2 <= 1.0) && (yap2 >= 0.0) && (yap2 <= 1.0) && (zap2 >= 0.0) && (zap2 <= 1.0);
+
+    /**
+     * Output data structure
+     * @property cx1 {number} - In canvas pixel coordinates, the x position of the start of the line
+     * @property cy1 {number} - In canvas pixel coordinates, the y position of the start of the line
+     * @property cz1 {number} - In canvas pixel coordinates, the z position of the start of the line
+     * @property cx2 {number} - In canvas pixel coordinates, the x position of the end of the line
+     * @property cy2 {number} - In canvas pixel coordinates, the y position of the end of the line
+     * @property cz2 {number} - In canvas pixel coordinates, the z position of the end of the line
+     * @property inside1_initial {boolean} - Flag indicating whether the start point was initially inside the plot area
+     * @property inside2_initial {boolean} - Flag indicating whether the end point was initially inside the plot area
+     * @property inside1_final {boolean} - Flag indicating whether the post-clipping start point is inside the plot area
+     * @property inside2_final {boolean} - Flag indicating whether the post-clipping end point is inside the plot area
+     */
     var output = {
         'cx1': x1, 'cy1': y1, 'cz1': z1,
         'cx2': x2, 'cy2': y2, 'cz2': z2,
-        'Inside1': ((xap1 >= 0.0) && (xap1 <= 1.0) && (yap1 >= 0.0) && (yap1 <= 1.0) && (zap1 >= 0.0) && (zap1 <= 1.0)),
-        'Inside2': ((xap2 >= 0.0) && (xap2 <= 1.0) && (yap2 >= 0.0) && (yap2 <= 1.0) && (zap2 >= 0.0) && (zap2 <= 1.0)),
-        'NCrossings': 0, 'AxisPos1': -1, 'AxisPos2': -1
+        'inside1_initial': inside1,
+        'inside2_initial': inside2,
+        'inside1_final': inside1,
+        'inside2_final': inside2
     };
-    if (output['Inside1'] && output['Inside2']) {
-        // If both points are inside canvas, don't need to find clip-region crossings
+
+    // If both points are inside canvas, don't need to find clip-region crossings
+    if (output['inside1'] && output['inside2']) {
         return output;
     }
 
-    var DOCLIP = function (XAP1, XAP2, YAP1, YAP2, ZAP1, ZAP2, POS, SGN) {
-        if (XAP2 !== XAP1) {
-            var fr = (POS - XAP1) / (XAP2 - XAP1);
-            if ((fr >= 0) && (fr <= 1)) {
-                var yleft = YAP1 + (YAP2 - YAP1) * fr;
-                var zleft = ZAP1 + (ZAP2 - ZAP1) * fr;
-                if ((yleft >= 0.0) && (yleft <= 1.0) && (zleft >= 0.0) && (zleft <= 1.0)) {
-                    var cx = x1 + (x2 - x1) * fr;
-                    var cy = y1 + (y2 - y1) * fr;
-                    var cz = z1 + (z2 - z1) * fr;
-                    if ((XAP1 * SGN) < (XAP2 * SGN)) {
+    /**
+     * do_clip - Perform clipping along the x axis only. Call repeatedly with different permutations of x, y, z to
+     * clip along all three axes.
+     * @param xap1 {number} - The fractional position along the x axis of the start point (in range 0 to 1)
+     * @param xap2 {number} - The fractional position along the x axis of the end point (in range 0 to 1)
+     * @param yap1 {number} - The fractional position along the y axis of the start point (in range 0 to 1)
+     * @param yap2 {number} - The fractional position along the y axis of the end point (in range 0 to 1)
+     * @param zap1 {number} - The fractional position along the z axis of the start point (in range 0 to 1)
+     * @param zap2 {number} - The fractional position along the z axis of the end point (in range 0 to 1)
+     * @param pos {number} - The position along the x axis to perform clipping. Either 0 or 1.
+     * @param sign {number} - Is this a minimum value of x (1), or a maximum value of x (-1)?
+     */
+    var do_clip = function (xap1, xap2, yap1, yap2, zap1, zap2, pos, sign) {
+        // Don't attempt clipping if no movement along x axis, to avoid numerical fail
+        if (xap2 !== xap1) {
+            // Fractional position in range 0 (start), 1 (end) where this line segment crosses clipping limit
+            var fraction = (pos - xap1) / (xap2 - xap1);
+
+            // Proceed if clipping limit is within the line segment
+            if ((fraction >= 0) && (fraction <= 1)) {
+                // Work out the fractional positions along the y and z axes where axis crossing occurs
+                var y_left = yap1 + (yap2 - yap1) * fraction;
+                var z_left = zap1 + (zap2 - zap1) * fraction;
+                // If axis crossing occurs within visible area, perform clipping
+                if ((y_left >= 0.0) && (y_left <= 1.0) && (z_left >= 0.0) && (z_left <= 1.0)) {
+                    // Work out canvas coordinates of point at the clipping limit
+                    var cx = x1 + (x2 - x1) * fraction;
+                    var cy = y1 + (y2 - y1) * fraction;
+                    var cz = z1 + (z2 - z1) * fraction;
+                    if ((xap1 * sign) < (xap2 * sign)) {
                         output['cx1'] = cx;
                         output['cy1'] = cy;
                         output['cz1'] = cz;
-                        output['AxisPos1'] = yleft;
+                        output['inside1_final'] = true;
                     } else {
                         output['cx2'] = cx;
                         output['cy2'] = cy;
                         output['cz2'] = cz;
-                        output['AxisPos2'] = yleft;
+                        output['inside2_final'] = true;
                     }
-                    output['NCrossings']++;
                 }
             }
         }
     };
 
-    DOCLIP(zap1, zap2, xap1, xap2, yap1, yap2, 0.0, 1.0); // front
-    DOCLIP(zap1, zap2, xap1, xap2, yap1, yap2, 1.0, -1.0); // back
-    DOCLIP(xap1, xap2, yap1, yap2, zap1, zap2, 0.0, 1.0); // left
-    DOCLIP(xap1, xap2, yap1, yap2, zap1, zap2, 1.0, -1.0); // right
-    DOCLIP(yap1, yap2, xap1, xap2, zap1, zap2, 0.0, 1.0); // bottom
-    DOCLIP(yap1, yap2, xap1, xap2, zap1, zap2, 1.0, -1.0); // top
+    // Clip at each end of each axis
+    do_clip(zap1, zap2, xap1, xap2, yap1, yap2, 0.0, 1.0); // front
+    do_clip(zap1, zap2, xap1, xap2, yap1, yap2, 1.0, -1.0); // back
+    do_clip(xap1, xap2, yap1, yap2, zap1, zap2, 0.0, 1.0); // left
+    do_clip(xap1, xap2, yap1, yap2, zap1, zap2, 1.0, -1.0); // right
+    do_clip(yap1, yap2, xap1, xap2, zap1, zap2, 0.0, 1.0); // bottom
+    do_clip(yap1, yap2, xap1, xap2, zap1, zap2, 1.0, -1.0); // top
 
     return output;
-
 };
 
 JSPlot_LineDraw.prototype.point = function (x, y, z, x_offset, y_offset, z_offset, x_perpoffset, y_perpoffset, z_perpoffset, linetype, linewidth) {
